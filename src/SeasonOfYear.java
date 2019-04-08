ae0ba6ae703177ec12c8eafa3652f98d830ad539
Svyat Dmitriev <sdmitr@softserveinc.com>
2019-04-08 14:23:23 +0300

    refactor SeasonOfYear

--- src/SeasonOfYear.java
+++ src/SeasonOfYear.java
@@ -3,44 +3,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SeasonOfYear {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please enter current Month:");
-        String input = reader.readLine();
-        Season season;
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Please enter current Month:");
+    String input = reader.readLine();
+    Season season = null;
 
-        switch (input) {
-        case "December":
-        case "January":
-        case "February":
-            season = Season.WINTER;
-            System.out.println("Current Season is:" + season);
-            break;
-        case "March":
-        case "April":
-        case "May":
-            season = Season.SPRING;
-            System.out.println("Current Season is:" + season);
-            break;
-        case "June":
-        case "July":
-        case "August":
-            season = Season.SUMMER;
-            System.out.println("Current Season is:" + season);
-            break;
-        case "September":
-        case "October":
-        case "November":
-            season = Season.AUTUMN;
-            System.out.println("Current Season is:" + season);
-            break;
-        default:
-            System.out.println("No such Month");
-        }
-
+    switch (input) {
+      case "December":
+      case "January":
+      case "February":
+        season = Season.WINTER;
+        break;
+      case "March":
+      case "April":
+      case "May":
+        season = Season.SPRING;
+        break;
+      case "June":
+      case "July":
+      case "August":
+        season = Season.SUMMER;
+        break;
+      case "September":
+      case "October":
+      case "November":
+        season = Season.AUTUMN;
+        break;
+      default:
+        System.out.println("No such Month");
     }
-
-    enum Season {
-        WINTER, SPRING, SUMMER, AUTUMN
+    if (season != null) {
+      System.out.println("Current Season is:" + season);
     }
+  }
+
+  enum Season {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN
+  }
 }
